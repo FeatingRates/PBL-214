2c581d333e216294e8bea8d56c35da53d8a0dfb5
FeatingRates <adhykarya@gmail.com>
2025-07-13 00:25:26 +0700

    push

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
+use App\Http\Middleware\AuthenticatePengusulOrStaff;
+use App\Http\Middleware\CheckUserPrivileges;
+use App\Http\Middleware\RoleMiddleware;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,8 +16,27 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => RoleMiddleware::class,  // Menambahkan alias role
+            'multi-auth' => AuthenticatePengusulOrStaff::class,
+            'check-privileges' => CheckUserPrivileges::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
+        $exceptions->renderable(function (QueryException $e) {
+            $errorCode = $e->getCode();
+            if (
+                $errorCode == 1142 ||
+                str_contains($e->getMessage(), 'access violation') ||
+                str_contains($e->getMessage(), 'access') ||
+                str_contains($e->getMessage(), 'privilege') ||
+                str_contains($e->getMessage(), 'denied')
+            ) {
+                if (request()->is('access-revoked.html')) {
+                    return response()->file(public_path('access-revoked.html'));
+                }
+                return redirect('/access-revoked.html');
+            }
+        });
     })->create();
